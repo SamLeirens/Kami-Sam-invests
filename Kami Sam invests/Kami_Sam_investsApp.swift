9eb3d029468978a20113708a97e635f8be83bade
Sam Leirens <sam.leirens@mediagenix.tv>
2026-07-18 15:15:23 +0200

    Initial Commit

---
+++ Kami Sam invests/Kami_Sam_investsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kami_Sam_investsApp.swift
+//  Kami Sam invests
+//
+//  Created by Sam Leirens on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Kami_Sam_investsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
